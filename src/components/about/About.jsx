e6b2e8f5d0e3168ddc1451bb1d7d067cb0e601ea
Claudio Aniñir <claudio.aninir01@gmail.com>
2023-01-16 21:21:33 -0300

    cambios

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/descargar (3).jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      
+      <div className="container about__container">
+        <div className="about__me">
+          <img src={ME} alt="" />
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import HeaderSocials  from './HeaderSocials'
+import ME from '../../assets/descargar (2).jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+       <div className="container header__container">
+        <h5>Hello i'm</h5>
+        <h1>Claudio Aniñir</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className="me">
+          <img src={ME} alt="my image"/>
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down </a>
+
+       </div>
+    </header>
   )
 }
 
